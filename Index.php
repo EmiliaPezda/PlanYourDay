149ae94af0f440d983afcbcf317e813aafb2db0e
Emilia P <emilia.ciejka@gmail.com>
2019-01-23 08:44:25 +0100

    geolocation panel

--- Index.php
+++ Index.php
@@ -12,19 +12,37 @@
   <body>
       
     <!--google maps -->
-    <div id="map">
+   <div id="map"></div>
     <script>
-      var map;
       function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 50.287063, lng: 21.4238101},
-          zoom: 8
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 8,
+          center: {lat: 50.287063, lng: 21.4238101}
+        });
+        var geocoder = new google.maps.Geocoder();
+
+        document.getElementById('submit').addEventListener('click', function() {
+          geocodeAddress(geocoder, map);
+        });
+      }
+
+      function geocodeAddress(geocoder, resultsMap) {
+        var address = document.getElementById('address').value;
+        geocoder.geocode({'address': address}, function(results, status) {
+          if (status === 'OK') {
+            resultsMap.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+              map: resultsMap,
+              position: results[0].geometry.location
+            });
+          } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+          }
         });
       }
     </script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDLI74YgPlEsEAMZHibswyGts1zFV7gfVk&callback=initMap"
-    async defer></script>
-    </div>
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDLI74YgPlEsEAMZHibswyGts1zFV7gfVk&callback=initMap"
+    ></script>
     
     <!-- User list -->
     <div id="list">
@@ -37,8 +55,8 @@
        Where are you going today?
        
        <form action="" id="routeForm">
-        <input type="text" id="routeStart" value="" placeholder="Input any address">
-        <input type="submit" value="Find">
+        <input type="text" id="address" value="" placeholder="Input any address">
+        <input type="submit" id="submit" value="Find">
         </form>
     </div>
     
